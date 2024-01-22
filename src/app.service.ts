b03989aee5bb63cc89cf0d526e8e5187dc621ae3
maratovv <maratovtemirmyrza@gmail.com>
2024-01-22 12:46:34 +0300

    Timaloh

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  calcAvg(){
+    const arr = [ 1 ,2, 3, 4, 5 ,6 ] 
+
+
+    let sum = 0 
+    for (let el of arr) {
+      sum += el 
+    }
+    
+    return sum / arr.length;
+  }
 }
